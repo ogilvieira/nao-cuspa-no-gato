9a37de60c1ea56ea82b48a37000e3ce3f27274eb
Gil Vieira <ogilvieira@gmail.com>
2016-11-02 14:50:29 -0200

    add score

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,10 @@ var game = new Phaser.Game(
 	GAP = 30,
 	AF = 0,
 	LIFE = 30,
-	cursors,
+	score = 0,
+	scoreStrig = '',
+	scoreText,
+	CURSORS,
 	actualPos = 2,
 	top,
 	footer,
@@ -33,25 +36,22 @@ function preload() {
 	game.load.image('gun', 'assets/img/gun.png');
 	game.load.image('btn', 'assets/img/btn.png');
 	game.load.image('split', 'assets/img/split.png');
-	// game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
 function create() {
-	cursors = game.input.keyboard.createCursorKeys();
+	CURSORS = game.input.keyboard.createCursorKeys();
     game.physics.startSystem(Phaser.Physics.ARCADE);
 	
+	// CREATE OBJECTS
 	top = new Phaser.Rectangle(0, 0, game.world.width, 60);
-	
-
-	btnLeft = game.add.button(5, game.world.height-110, 'btn', function(){ console.log('LEFT') }, this, 2, 3, 0);
-	btnRight = game.add.button(325, game.world.height-110, 'btn', function(){ console.log('RIGHT') }, this, 2, 3, 0);
-
     PLAYER = game.add.sprite((actualPos*120)+20, game.world.height - 230, 'cat');
     GUN = game.add.sprite((actualPos*120)+20, 60, 'gun');
+    SPLIT = game.add.group();
 
     game.physics.arcade.enable(PLAYER);
     game.physics.arcade.enable(GUN);
 
+	// SET OBJECTS
     PLAYER.body.collideWorldBounds = true;
     PLAYER.body.gravity.x = false;
     PLAYER.body.bounce.x = 0;
@@ -62,17 +62,22 @@ function create() {
     GUN.body.bounce.x = 0;
     GUN.body.gravity.x = 0;
 
-
-    SPLIT = game.add.group();
     SPLIT.enableBody = true;
     SPLIT.physicsBodyType = Phaser.Physics.ARCADE;
-	// SPLIT.body.gravity.y = 600;
+	
+	// CREATE BUTTONS
+	btnLeft = game.add.button(5, game.world.height-105, 'btn', function(){ console.log('LEFT') }, this, 2, 3, 0);
+	btnRight = game.add.button(325, game.world.height-105, 'btn', function(){ console.log('RIGHT') }, this, 2, 3, 0);
+
+
+    //  The score
+    scoreString = 'Score : ';
+    scoreText = game.add.text(10, 10, scoreString + score, { font: '34px Arial', fill: '#fff' });
 }
 
 
 function render () {
 	game.debug.geom(top, "#474747");
-	game.debug.geom(footer, "#FFFFFF");
 }
 
 
@@ -87,15 +92,17 @@ function update() {
 
         var pineapple = SPLIT.create(shotpos+40, 120, 'split');
         pineapple.body.collideWorldBounds = false;
-        pineapple.body.gravity.y = 600;
+        pineapple.body.gravity.y = 1000;
+        pineapple.checkWorldBounds = true;
+        pineapple.events.onOutOfBounds.add(splitOut, this);
 	}
 
 	game.physics.arcade.collide(PLAYER, SPLIT, collisionHandler, null, this);
 
-	if(cursors.left.isDown && !cursors.left.repeats){
+	if(CURSORS.left.isDown && !CURSORS.left.repeats){
 		if(actualPos > 0){ actualPos-- }
 	}
-	else if (cursors.right.isDown && !cursors.right.repeats)
+	else if (CURSORS.right.isDown && !CURSORS.right.repeats)
 	{
 		if(actualPos < 4){ actualPos++ }
 	}
@@ -108,4 +115,9 @@ function collisionHandler (obj1, obj2) {
 	obj2.body.allowGravity = false;
 	obj2.destroy(true);
 	console.log(LIFE);
+}
+
+function splitOut(split) {
+	score++;
+	scoreText.text = scoreString + score;
 }
